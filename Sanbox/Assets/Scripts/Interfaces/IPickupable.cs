942f4c229e389f921b6cd8d275b2e6f319242afa
thejovialknight <thejovialknight@gmail.com>
2019-11-20 18:20:45 -0800

    Renamed project

--- Sanbox/Assets/Scripts/Interfaces/IPickupable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IPickupable
-{
-    bool IsActive { get; set; }
-    void Pickup(GameObject bitbot);
-}
