e426885903095756a0bcd71bee5572e1e20a8459
thejovialknight <thejovialknight@gmail.com>
2019-11-25 19:21:47 -0800

    Changed selection?
    Not sure if merge will break.

--- Assets/Scripts/SelectionController.cs
+++ Assets/Scripts/SelectionController.cs
@@ -1,15 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SelectionController : MonoBehaviour
 {
-    GameObject selection = null;
-
-    public Transform selectorPrefab;
-
-    public List<Transform> currentSelection = new List<Transform>();
-    public List<Selector> currentSelector = new List<Selector>(); // HAND CONTROL OF SELECTOR OBJECTS OVER TO THOSE WHO ARE SELECTED PERCHANCE? ANOTHER SCRIPT TO HANDLE THIS ATTACHED TO SELECTED GAME OBJECT.
+    // HAND CONTROL OF SELECTOR OBJECTS OVER TO THOSE WHO ARE SELECTED PERCHANCE? ANOTHER SCRIPT TO HANDLE THIS ATTACHED TO SELECTED GAME OBJECT.
 
     /*
      * SELECTIONS DEFINE THEIR BEHAVIOR WITH RESPECT TO CONTROLS USING INTERFACES
@@ -35,26 +31,16 @@ public class SelectionController : MonoBehaviour
 
     void Select(RaycastHit hit)
     {
-        if (!GameObject.ReferenceEquals(hit.transform, currentSelection))
+        if (hit.transform.CompareTag("Terrain"))
         {
-            if (selection != null)
-            {
-                DeselectObject();
-            }
+            EventManager.DeselectEvent();
+        }
 
-            /* REPLACE THESE IFS WITH CHECKS FOR INTERFACES
-            if (hit.collider.CompareTag("Bitbot"))
-            {
-                SelectObject(hit.collider.gameObject, 1.5f, 0.25f);
-
-                // make bitbot do this when event is fired. In fact, this will allow the selector to be entirely programmatic
-                selection.GetComponent<PlayerNavController>().enabled = true;
-            }
-            else if (hit.collider.CompareTag("Structure"))
-            {
-                SelectObject(hit.collider.gameObject, 5f, 0.125f);
-            }
-            */
+        ISelectable selectable = hit.transform.GetComponent<ISelectable>();
+        if(selectable != null)
+        {
+            GameObject[] objects = { hit.transform.gameObject };
+            EventManager.SelectEvent(objects);
         }
     }
 
@@ -75,31 +61,5 @@ public class SelectionController : MonoBehaviour
             navPoint.GetComponent<StructureNavPoint>().structure = hit.collider.gameObject;
         }
         */
-    }
-
-    void SelectObject(GameObject obj, float selectorSize, float yOffset)
-    {
-        //currentSelection = obj.transform;
-        //currentSelector = GameObject.Instantiate(selectorPrefab).GetComponent<Selector>();
-        //currentSelector.Initialize(obj.transform, selectorSize, yOffset);
-        selection = obj;
-        EventManager.SelectEvent(selection);
-    }
-
-    void DeselectObject()
-    {
-        EventManager.DeselectEvent(selection);
-        if(currentSelector != null) {
-            //currentSelector.Destroy();
-            currentSelection = null;
-            currentSelector = null;
-        }
-
-        // must relegate this elsewhere as well if im honest
-        PlayerNavController navController = selection.GetComponent<PlayerNavController>();
-        if (navController != null)
-        {
-            navController.enabled = false;
-        }
     }
 }
